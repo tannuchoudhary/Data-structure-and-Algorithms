69f025889159f8559d1817b7a536a4920eb14282
TANNU KUMARI <tannuchoudhary10@gmail.com>
2019-12-07 22:21:23 +0530

    Create 1.EmptyTree.cpp

---
+++ Tree/1.EmptyTree.cpp
@@ -0,0 +1,20 @@
+/*--------------CREATING AN EMPTY TREE---------------------------*/
+#include<iostream>
+using namespace std;
+/*Below is the structure for node of a tree, any node of a tree will
+ contain data, left pointer(for containing the address of left subtree)
+  and right pointer(for containing the address of a right subtree.)
+ */
+struct treeNode{ 
+  int data;
+  treeNode *right;
+  treeNode *left;  
+}
+int main(){
+  /*For creating a tree you need a root pointer just like head poimter in linked list*/
+  
+  /*Initially your tree is empty so  root pointer will not contain any address i.e it will contain NULL*/
+  
+  treeNode *root = NULL;
+  return 0;
+}
